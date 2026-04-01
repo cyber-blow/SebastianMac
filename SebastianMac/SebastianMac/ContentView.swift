eea63f0303cc4d47fa363d67bf0773e179f9a338
yuki <yuki@yukinoMacBook-Pro.local>
2026-04-02 00:42:35 +0900

    Initialize macOS Xcode project

---
+++ SebastianMac/SebastianMac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SebastianMac
+//
+//  Created by yuki on 2026/04/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SebastianMac/SebastianMac/SebastianMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  SebastianMacApp.swift
+//  SebastianMac
+//
+//  Created by yuki on 2026/04/02.
+//
+
+import SwiftUI
+
+@main
+struct SebastianMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
